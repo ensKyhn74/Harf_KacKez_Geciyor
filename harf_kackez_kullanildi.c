8118cfe15f747386b569978e7a427868319c1977
ensKyhn74 <117343032+ensKyhn74@users.noreply.github.com>
2023-02-04 22:52:50 +0300

    Add files via upload

---
+++ harf_kackez_kullanildi.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+// klavyeden metin girilsin.Kullanıcıya hangi harfin kaç kez geçtiğini bulmak istediği sorulsun
+// input: bilgisayar     adeti istenen harf: i    output: 2
+
+int main()
+
+{
+  
+ char metin[50];
+ int i = 0;
+ int HarfSayaci = 0;
+ char harf;
+ 
+ printf("metin girin: ");
+ gets(metin);
+ 
+ printf("hangi harfin kac kez gectigini bulmak istersiniz?: ");
+ scanf("%c",&harf);
+ 
+ while(metin[i] != '\0')
+ {
+
+  if(metin[i] == harf) 
+  {
+    HarfSayaci++;
+  }
+     
+  i++;  
+   
+ }
+ 
+ printf("%c harfi metinde %d kez gecmektedir",harf,HarfSayaci);
+
+ return 0;
+ 
+}
